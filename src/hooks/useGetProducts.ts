13469daa0b43cfefb0ac7350b5490f23c86d0f87
Mohmd <mim.mohrechi@gmail.com>
2024-04-18 20:03:15 +0330

    added fake delay for demonstrating loading state

--- src/hooks/useGetProducts.ts
+++ src/hooks/useGetProducts.ts
@@ -1,4 +1,4 @@
 export const useGetProducts = async () => {
+    await new Promise((res) => setTimeout(res, 3000));
     return fetch("https://fakestoreapi.com/products").then((res) => res.json());
 };
-
