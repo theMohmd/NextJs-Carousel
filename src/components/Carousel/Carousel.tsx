4f4345451340ceddc8e04c24dc8a14ab5badece5
Mohmd <mim.mohrechi@gmail.com>
2024-04-18 18:02:36 +0330

    finished carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,16 +1,56 @@
+"use client";
+
 import { apiProductType } from "@/types/apiProduct";
 import CarouselItem from "./CarouselItem";
+import { useState } from "react";
+import { RightChevronIcon, LeftChevronIcon } from "../ui/Icons";
 
 type CarouselProps = {
     data: apiProductType[];
 };
 
+const ClassName = "text-green-800 w-8 absolute top-0 h-full hover:bg-opacity-20 hover:bg-green-800";
 const Carousel = ({ data }: CarouselProps) => {
+    const [index, setindex] = useState(0);
+    const prevAction = () => {
+        setindex((prev) => {
+            if (prev > 0) return prev - 1;
+            else return data.length - 1;
+        });
+    };
+    const nextAction = () => {
+        setindex((prev) => {
+            if (prev < data.length - 1) return prev + 1;
+            else return 0;
+        });
+    };
     return (
-        <div>
-            {data.map((item) => (
-                <CarouselItem key={item.id} data={item} />
-            ))}
+        <div className="flex overflow-hidden relative p-5 px-8 w-full bg-white rounded-xl aspect-[2/3] max-w-[800px] md:aspect-[2]">
+            <div className="size-full [&>*]:grow-0 [&>*]:shrink-0 flex overflow-hidden">
+                {data.map((item) => (
+                    <div
+                        key={item.id}
+                        style={{ translate: `${-100 * index}%` }}
+                        className="duration-500 size-full"
+                    >
+                        <CarouselItem data={item} />
+                    </div>
+                ))}
+            </div>
+            <button
+                className={ClassName}
+                style={{ right: 0 }}
+                onClick={nextAction}
+            >
+                <RightChevronIcon />
+            </button>
+            <button
+                className={ClassName}
+                style={{ left: 0 }}
+                onClick={prevAction}
+            >
+                <LeftChevronIcon />
+            </button>
         </div>
     );
 };
--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import { apiProductType } from "@/types/apiProduct";
 import Image from "next/image";
+import { CartIcon } from "../ui/Icons";
 
 type CarouselItemProps = { data: apiProductType };
 
 const CarouselItem = ({ data }: CarouselItemProps) => {
     return (
-        <div>
-            <Image src={data.image} width={100} height={100} alt={data.title} />
-            <p>{data.title}</p>
-            <p>{data.price}</p>
-            <p>{data.description}</p>
+        <div className="grid md:grid-cols-2 grid-rows-2 md:grid-rows-1 size-full">
+            <div className="flex justify-center items-center p-5">
+                <Image
+                    className="object-contain size-full"
+                    src={data.image}
+                    width={400}
+                    height={400}
+                    alt={data.title}
+                />
+            </div>
+            <div className="flex flex-col justify-center p-5">
+                <p className="text-2xl font-bold line-clamp-2">{data.title}</p>
+                <p className="mt-2 text-neutral-700 line-clamp-6 ">{data.description}</p>
+                <div className="flex justify-between items-center mt-8">
+                    <p className="text-2xl font-bold text-green-800">
+                        {data.price}$
+                    </p>
+                    <button className="flex gap-1 items-center py-2 px-4 w-auto text-white bg-green-800 rounded-md">
+                        <p className="font-medium">Buy Now</p>
+                        <div className="size-7">
+                            <CartIcon />
+                        </div>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
