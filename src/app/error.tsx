7bfd5e95679adc9dfa960b5b27312764bb06a772
Mohmd <mim.mohrechi@gmail.com>
2024-04-18 20:19:29 +0330

    added error component for home page

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+const Error = () => {
+    return (
+        <div className="flex justify-center items-center p-5 h-dvh">
+            <div className="flex justify-center items-center w-full text-3xl font-bold bg-white rounded-xl aspect-[2/3] max-w-[800px] text-red-600 md:aspect-[2]">
+                Something went wrong!
+            </div>
+        </div>
+    );
+};
+
+export default Error;
