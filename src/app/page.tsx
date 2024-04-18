2dd088914d50106b455944f967707cc063ff817e
Mohmd <mim.mohrechi@gmail.com>
2024-04-18 18:03:16 +0330

    polished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Carousel from "@/components/Carousel/Carousel";
+import { useGetProducts } from "@/hooks/useGetProducts";
+import { apiProductType } from "@/types/apiProduct";
 
-export default function Home() {
-  return (
-    <main>Next</main>
-  );
-}
+const Home = async () => {
+    const data: apiProductType[] = await useGetProducts();
+    return (
+        <div className="flex justify-center items-center background h-dvh p-5">
+            <Carousel data={data} />
+        </div>
+    );
+};
+
+export default Home;
